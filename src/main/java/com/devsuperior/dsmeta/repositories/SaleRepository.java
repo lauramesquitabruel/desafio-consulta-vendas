e5e41aa275075af0238014d63880f6596def609c
lauramesquitabruel <lauramesquitabruel@gmail.com>
2025-02-06 15:05:20 -0300

    endopoint /summary

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -14,7 +14,7 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
     @Query(nativeQuery = true, value = "SELECT t1.name AS sellerName, SUM(t2.amount) AS total\n" +
             "FROM TB_SELLER t1 \n" +
             "JOIN TB_SALES t2 ON t1.id = t2.seller_id\n" +
-            "WHERE t2.date BETWEEN ':minDate' AND ':maxDate'\n" +
+            "WHERE t2.date BETWEEN :minDate AND :maxDate\n" +
             "GROUP BY sellerName")
     List<SummaryProjection> summary(LocalDate minDate, LocalDate maxDate);
 }
