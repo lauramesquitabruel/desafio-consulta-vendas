28fd688774d55ae7f3ca8550942567f34c7410aa
lauramesquitabruel <lauramesquitabruel@gmail.com>
2025-02-06 12:04:11 -0300

    conversão de datas String para LocalDate

--- src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,14 +1,14 @@
 package com.devsuperior.dsmeta.controllers;
 
+import com.devsuperior.dsmeta.dto.SummaryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.services.SaleService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -30,8 +30,9 @@ public class SaleController {
 	}
 
 	@GetMapping(value = "/summary")
-	public ResponseEntity<?> getSummary() {
-		// TODO
-		return null;
+	public ResponseEntity<List<SummaryDTO>> getSummary(@RequestParam(defaultValue = "") String minDate,
+													  @RequestParam(defaultValue = "") String maxDate) {
+		List<SummaryDTO> summaryDTO = service.getSummary(minDate, maxDate);
+		return ResponseEntity.ok(summaryDTO);
 	}
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import com.devsuperior.dsmeta.dto.SummaryDTO;
@@ -25,7 +28,21 @@ public class SaleService {
 		return new SaleMinDTO(entity);
 	}
 
-	public List<SummaryDTO> getSummary(LocalDate minDate, LocalDate maxDate){
+	public List<SummaryDTO> getSummary(String minDateText, String maxDateText){
+		LocalDate minDate;
+		LocalDate maxDate;
+
+		if(Objects.equals(maxDateText, "")){
+			maxDate = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		} else {
+			maxDate = LocalDate.parse(maxDateText);
+		}
+
+		if(Objects.equals(minDateText, "")){
+			minDate = maxDate.minusYears(1L);
+		} else {
+			minDate = LocalDate.parse((minDateText));
+		}
 		List<SummaryProjection> result = repository.summary(minDate, maxDate);
 		return result.stream().map(SummaryDTO::new).toList();
 	}
